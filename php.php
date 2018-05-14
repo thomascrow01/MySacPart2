b7db696b23a74adec6d2cb162a0c3b9b50995760
Thomas Crow <thomascrow01@gmail.com>
2018-05-15 01:15:43 +1000

    Outdated and not needed

--- php.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$totalmass = $workermass + $belongingsmass;
-	$reassembliumcost = $totalmass * 4000;
-	$unassembliumcost = $totalmass * 1000;
-	$totalcost = $reassembliumcost + $unassembliumcost;
-?>
