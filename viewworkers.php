6853cfe49b8ad80513333440bf459fc8eb8cd38a
Thomas Crow <thomascrow01@gmail.com>
2018-05-15 18:15:47 +1000

    Added link to add a worker

--- viewworkers.php
+++ viewworkers.php
@@ -7,9 +7,10 @@
 	<header>
 		<a href="index.html"><div class="headerbutton"><h2>Home</h2></div></a>
 	</header>
+	<div><h2><a href="addworker1.html">Add a new worker.</a></h2></div>
 	<?php
 		/* Create first line of the table */
-		echo '<table border="1"><tr><td>Name</td><td>Worker Mass</td><td>Belongings Mass</td></tr>';
+		echo '<table border="1"><tr><td>Name</td><td>Worker Mass</td><td>Belongings Mass</td><td>Cost of a 1 way trip</td><td>Unassemblium required</td><td>Reassemblium required</td><td>Cost of belongings</td><td>Cost of unassemblium</td><td>Cost of reassemblium</td><td>Cost of two way trip</td><td>Total mass</td></tr>';
 		/* Open file */
 		$file = fopen("worker.txt", "r") or die("worker.txt could not be opened");
 		while (!feof($file)){   
