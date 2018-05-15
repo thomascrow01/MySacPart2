ea0bd84b1fd32feb55f3c2938fe61f9aa6b59fb7
Thomas Crow <thomascrow01@gmail.com>
2018-05-15 18:50:36 +1000

    added excess luggage

--- viewworkers.php
+++ viewworkers.php
@@ -10,7 +10,7 @@
 	<div><h2><a href="addworker1.html">Add a new worker.</a></h2></div>
 	<?php
 		/* Create first line of the table */
-		echo '<table border="1"><tr><td>Name</td><td>Worker Mass</td><td>Belongings Mass</td><td>Cost of a 1 way trip</td><td>Unassemblium required</td><td>Reassemblium required</td><td>Cost of belongings</td><td>Cost of unassemblium</td><td>Cost of reassemblium</td><td>Cost of two way trip</td><td>Total mass</td></tr>';
+		echo '<table border="1"><tr><td>Name</td><td>Worker Mass</td><td>Belongings Mass</td><td>Cost of a 1 way trip</td><td>Unassemblium required</td><td>Reassemblium required</td><td>Cost of belongings</td><td>Cost of unassemblium</td><td>Cost of reassemblium</td><td>Cost of two way trip</td><td>Total mass</td><td>Cost of excess luggage</td></tr>';
 		/* Open file */
 		$file = fopen("worker.txt", "r") or die("worker.txt could not be opened");
 		while (!feof($file)){   
